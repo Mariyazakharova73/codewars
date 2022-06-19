d7c620d298a8873bd4804ef14e8feff21eeb2917
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-06-20 00:07:22 +0400

    feat: Arrays Similar

--- 6 kyu.js
+++ 6 kyu.js
@@ -64,7 +64,7 @@ function getConsectiveItems(items, key) {
   return imax;
 }
 
-//Arrays Similar
+// https://www.codewars.com/kata/51e704f2d8dbace389000279/javascript
 function arraysSimilar(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
