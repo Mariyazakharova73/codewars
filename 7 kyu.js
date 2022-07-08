dbafe925b495868ab56b2eea78c0be4c973560e7
Mariya Zakharova <zakharovamaria73@yandex.ru>
2022-07-08 20:38:07 +0400

    feat: Find the middle element

--- 7 kyu.js
+++ 7 kyu.js
@@ -177,3 +177,8 @@ var typeOfTriangle = function (sideA, sideB, sideC) {
     }
   }
 };
+
+//https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript
+function gimme(a) {
+  return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+}
